5071cce5f9f7ea103688e80f6f045c6ed2232a7b
Васильев <workgames@mail.ru>
2021-07-09 18:13:43 +0500

    ДЗ №1: консольный калькулятор

---
+++ scripts/calc/RPNTransform.ts
@@ -0,0 +1,116 @@
+import { mathOperatorsIds, mathOperatorsPriorities } from "./operators";
+
+class RPNTransform {
+
+  isValid(val: string): boolean {
+    return !isNaN(parseFloat(val)) || this.isOperator(val);
+  }
+
+  private leftAssoc(operator: string): boolean {
+    return operator !== "^";
+  }
+
+  private priority(operator: string): number {
+    return mathOperatorsPriorities[operator] || 0;
+  }
+
+  private isOperator(operator: string): boolean {
+    return !!mathOperatorsPriorities[operator];
+  }
+
+  toPostfix(expr: string): string {
+    let i = 0;
+
+    const nextToken = (): string => {
+      while (i < expr.length && expr[i] == " ") {
+          i++;
+      }
+
+      if (i == expr.length) {
+          return "";
+      }
+
+      let b = "";
+      
+      while (
+        i < expr.length &&
+        expr[i] != " " &&
+        expr[i] != "(" &&
+        expr[i] != ")" &&
+        !this.isOperator(expr[i])
+      ) {
+        b += expr[i++];
+      }
+
+      if (b != "") {
+        return b;
+      }
+
+      return expr[i++];
+    };
+
+    let S: Array<string> = [];
+    let O: Array<string | undefined> = [];
+    let tok: string;
+    let prevTok: string = '';
+
+    while ((tok = nextToken()) != "") {
+      if (this.isOperator(tok) && this.isOperator(prevTok)) {
+        if (mathOperatorsIds[tok] === mathOperatorsIds[prevTok]) {
+          throw new Error("Ввели несколько операторов подряд");
+        }
+      }
+      prevTok = tok;
+      if (tok == "(") {
+        S.push(tok);
+      } else if (tok == ")") {
+
+        while (S.length > 0 && S[S.length - 1] != "(") {
+          O.push(S.pop());
+        }
+
+        if (S.length == 0) {
+          throw new Error("Несоответствующие скобки или оператор");
+        }
+
+        S.pop();
+      } else if (this.isOperator(tok)) {
+
+        while (
+          S.length > 0 &&
+          this.isOperator(S[S.length - 1]) &&
+          ((this.leftAssoc(tok) &&
+            this.priority(tok) <= this.priority(S[S.length - 1])) ||
+            (!this.leftAssoc(tok) &&
+              this.priority(tok) < this.priority(S[S.length - 1])))
+        )
+        O.push(S.pop());
+        S.push(tok);
+      } else {
+        O.push(tok);
+      }
+    }
+
+    while (S.length > 0) {
+      if (!this.isOperator(S[S.length - 1])) {
+        throw new Error("Несоответствующие скобки или оператор");
+      }
+      O.push(S.pop());
+    }
+    if (O.length == 0) {
+      throw new Error("Ошибка ввода");
+    }
+
+    let s = "";
+    for (let i = 0; i < O.length; i++) {
+      if (!this.isValid(O[i] as string)) {
+        throw new Error("Несоответствующие скобки или оператор");
+      }
+      if (i != 0) s += " ";
+      s += O[i];
+    }
+    return s;
+  }
+}
+
+export const rpnTransform = new RPNTransform();
